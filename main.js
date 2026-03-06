dc2a02462ab2113581cb46366d180b318a3202cd
Cocepter Bosibori <bosiboriconcepter501@gmail.com>
2026-03-06 21:01:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+let name = "Connie";
+let age = 18;
+const birthYear = 2008;
+
+// typeof operator
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;  // Works!
+
+const PI = 3.14159;
+// PI = 3;  // Error! const cannot be reassigned
 
